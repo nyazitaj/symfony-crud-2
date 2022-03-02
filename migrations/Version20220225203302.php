9d7b16dea58ce9ff88bcd2f785300c5ff98da65d
nyazitaj <100127453+nyazitaj@users.noreply.github.com>
2022-03-02 22:27:15 +0100

    Complete project

---
+++ migrations/Version20220225203302.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220225203302 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE films CHANGE title title VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE realisateur realisateur VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE genre genre VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE image image VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE link link VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE description description VARCHAR(2000) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE status status VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE seance CHANGE lang lang VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE duree duree VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE status status VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE user CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE email email VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE password password VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
--- src/Controller/FilmsController.php
+++ src/Controller/FilmsController.php
@@ -26,21 +26,8 @@ class FilmsController extends AbstractController
      */
     public function film_read( ManagerRegistry $doctrine): Response
     {
-        /* $films = $this->getDoctrine()
-            ->getRepository(films::class)
-            ->find($id);
-
-        if (!$films) {
-            throw $this->createNotFoundException(
-                'The film list is empty '.$id
-            );
-        } */
 
         $films = $doctrine -> getManager() -> getRepository(films::class) -> findAll();
-
-        /* if( !isset($films) ) {
-            return $this -> redirectToRoute('acceuil');
-        } */
 
         return $this->render('films/films-read.html.twig', [
             'films' => $films,
@@ -90,28 +77,6 @@ class FilmsController extends AbstractController
             $film = new Films;
         }
 
-
-        /* $film = [
-            'title'
-        ]; */
-
-        // $film = new Films;
-
-        // $film -> setTitle('Les dents de la mer');
-        // $film -> setRealisateur('Steven Spilberg');
-        // $film -> setGenre('Horreur');
-
-        // $entityManager -> persist($film);
-        // $entityManager -> flush();
-
-        // return new Response('Saved new films with id '.$film->getTitle());
-
-        /* return $this->render('films/film-add.html.twig', [
-            'film_title' => $film->getTitle(),
-        ]); */
-
-
-
         $form = $this->createFormBuilder($film)
             ->add('title', TextType::class)
             ->add('realisateur', TextType::class)
@@ -127,9 +92,6 @@ class FilmsController extends AbstractController
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
-
 
             if(!$film -> getId()) {
                 $film -> setCreatedAt( new DateTimeImmutable() );
@@ -144,9 +106,6 @@ class FilmsController extends AbstractController
             $entityManager->flush();
 
             return $this->redirectToRoute('film_read');
-            /* return $this->render('films/film-add.html.twig', [
-                'form' => $form -> createView(),
-            ]); */
         }
 
 
@@ -156,6 +115,104 @@ class FilmsController extends AbstractController
             'form' => $form -> createView(),
             'idEditor' => $idEditor
         ]);
+    }
+
+
+    // Saving default film list in database (not working)
+    /**
+     * @Route("/film_default", name="film_default")
+     */
+    public function addDefaultFilmList( ManagerRegistry $doctrine, Request $request, $id=null): Response
+    {
+
+
+        // Adding default values in database.
+        $filmArray = [
+            [
+                'title' => 'Infiltrator',
+                'realisateur' => 'Brad Furman',
+                'genre' => 'Thriller',
+                'affiche' => '/assets/images/film-posters/Infiltrator.jpg',
+                'link' => 'https://57webbee.com',
+                'resume' => "L’agent fédéral Bob Mazur a pour mission d’infiltrerle cartel de drogue de Pablo Escobar.Son but : faire tomber 85 barons et une banqueinternationale.Son plan : s’inventer un passé, une identité, unefiancée.Son risque : le moindre faux pas lui serait fatal.",
+                'duree' => '127',
+                'status' => 'Disponible',
+            ],
+            [
+                'title' => 'Brain Freeze',
+                'realisateur' => 'Julien Knafo',
+                'genre' => 'Comédie, Epouvante-horreur',
+                'affiche' => '/assets/images/film-posters/mthumb.jpg',
+                'link' => 'https://57webbee.com',
+                'resume' => "Un engrais utilisé dans une riche communauté fermée devient la source d'une mutation génétique qui transforme ses habitants en zombies. Un adolescent et sa petite sœur peuvent-ils s'échapper de l'île en quarantaine avant de se transformer en herbe ?",
+                'duree' => '91',
+                'status' => 'Disponible',
+            ],
+            [
+                'title' => 'See for Me',
+                'realisateur' => 'Randall Okita',
+                'genre' => 'Epouvante-horreur',
+                'affiche' => '/assets/images/film-posters/see-for-me.jpg',
+                'link' => 'https://57webbee.com',
+                'resume' => "Sophie, une jeune femme aveugle, vit dans un manoir isolé, se retrouve prise au piège par des voleurs qui cherchent un coffre-fort caché. Son seul moyen de défense : une nouvelle application appelée \"See For Me\". Elle la connecte à un volontaire à travers le pays qui l'aide à survivre en voyant à sa place. Sophie est mise en relation avec Kelly, un vétéran de l'armée qui passe ses journées à jouer à des jeux vidéos de guerre.",
+                'duree' => '92',
+                'status' => 'Disponible',
+            ],
+            [
+                'title' => 'Halloween Kills',
+                'realisateur' => 'David Gordon Green',
+                'genre' => 'Epouvante-horreur',
+                'affiche' => '/assets/images/film-posters/halloween.png',
+                'link' => 'https://57webbee.com',
+                'resume' => "Laurie Strode, sa fille Karen et sa petite fille Allyson viennent d’abandonner le monstre au célèbre masque, enfermé dans le sous-sol de la maison dévorée par les flammes. Grièvement blessée, Laurie est transportée en urgence à l’Hôpital, avec la certitude qu’elle vient enfin de se débarrasser de celui qui la harcèle depuis toujours. Mais Michael Myers parvient à s’extirper du piège où Laurie l’avait enfermé et son bain de sang rituel recommence. Surmontant sa douleur pour se préparer à l’affronter encore une fois, elle va inspirer la ville entière qui décide de l’imiter et de se soulever pour exterminer ce fléau indestructible. Les trois générations de femmes vont s’associer à une poignée de survivants du premier massacre, et prennent les choses en main en formant une milice organisée autour de la chasse et la destruction du monstre une fois pour toutes. Le mal meurt cette nuit.",
+                'duree' => '106',
+                'status' => 'Disponible',
+            ],
+            /* [
+                'title' => '',
+                'realisateur' => '',
+                'genre' => '',
+                'affiche' => '/assets/images/film-posters/mthumb.jpg',
+                'link' => 'https://57webbee.com',
+                'resume' => "",
+                'duree' => '',
+                'status' => 'Disponible',
+            ],
+            [
+                'title' => '',
+                'realisateur' => '',
+                'genre' => '',
+                'affiche' => '/assets/images/film-posters/mthumb.jpg',
+                'link' => 'https://57webbee.com',
+                'resume' => "",
+                'duree' => '',
+                'status' => 'Disponible',
+            ], */
+        ];
+
+        $films = new Films;
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        foreach ( $filmArray as $vls ) {
+            $films -> setTitle( $vls['title'] );
+            $films -> setRealisateur( $vls['realisateur'] );
+            $films -> setGenre( $vls['genre'] );
+            $films -> setImage( $vls['affiche'] );
+            $films -> setLink( $vls['link'] );
+            $films -> setDescription( $vls['resume'] );
+            $films -> setDuree( $vls['duree'] );
+            $films -> setStatus( $vls['status'] );
+            $films -> setCreatedAt(  new DateTimeImmutable() );
+            $films -> setUpdatedAt(  new DateTimeImmutable() );
+
+            $entityManager->persist($films);
+        }
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('film_read');
+
     }
 
     /**
